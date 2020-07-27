3a61e0757a0535d6520a2972bf1a774de28ab1f6
Kevin Atienza <kevin.charles.atienza@gmail.com>
2020-07-28 00:50:09 +0800

    add KG user and contest loaders

--- cmscontrib/loaders/kompgen.py
+++ cmscontrib/loaders/kompgen.py
@@ -21,13 +21,11 @@ import json
 import logging
 import os
 import stat
-from datetime import timedelta#, datetime
+from datetime import timedelta, datetime
 
-# from cms import config
 from cms.db import Contest, User, Task, Statement, Attachment, Dataset, Manager, Testcase
 from cmscommon.constants import SCORE_MODE_MAX, SCORE_MODE_MAX_SUBTASK, SCORE_MODE_MAX_TOKENED_LAST
-# from cmscommon.crypto import build_password
-# from cmscontrib import touch
+from cmscommon.crypto import build_password
 from .base_loader import ContestLoader, TaskLoader, UserLoader
 
 
@@ -67,6 +65,34 @@ TASK_DEFAULTS = {
     'score_mode': SCORE_MODE_MAX_SUBTASK,
 }
 
+LANGUAGE_MAPPING = {
+    'c++': 'C++11 / g++',
+    'cpp': 'C++11 / g++',
+    'java': 'Java / JDK',
+    'python3': 'Python 3 / CPython',
+    'pypy3': 'Python 3 / PyPy',
+}
+
+CONTEST_DEFAULTS = {
+    'name': None,
+    'description': None,
+    'languages': ['cpp', 'java', 'python3'],
+    'submissions_download_allowed': True,
+    'allow_questions': True,
+    'allow_user_tests': True,
+    'block_hidden_participations': False,
+    'allow_password_authentication': True,
+    'start': None,
+    'stop': None,
+    'timezone': None,
+    'per_user_time': None,
+    'max_submission_number': 500,
+    'max_user_test_number': 100,
+    'min_submission_interval': 1,
+    'min_user_test_interval': 1,
+    'score_precision': 0,
+}
+
 def make_executable(filename):
     os.chmod(filename, os.stat(filename).st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
 
@@ -75,8 +101,8 @@ class KGTaskLoader(TaskLoader):
     """
     Load a task stored using the KompGen cms-compiled format.
 
-    Given the filesystem location of a task prepared via KompGen and
-    compiled via `kg make allkg kompile cms` (by default located at
+    Given the filesystem location of a task prepared via KompGen and compiled
+    via `kg make all` and then kg kompile cms` (by default located at
     `kgkompiled/cms`), parse the contents to produce a CMS Task object.
 
     Not all options are supported yet.
@@ -94,21 +120,22 @@ class KGTaskLoader(TaskLoader):
 
     def _get_task_config(self):
         kg_task = os.path.join(self.path, KG_TASK)
-        logger.info(f"Reading {kg_task}...")
+        logger.info(f"Reading {kg_task}")
         with open(kg_task) as file:
             return json.load(file)
 
     def _get_task(self, task_config, get_statement=True):
 
-        logger.info("Creating the Task object...")
-
+        # initialize the basic fields
+        logger.info("Creating the Task object")
         task_fields = {field: task_config.get(field, default) for field, default in TASK_DEFAULTS.items()}
 
         name = task_fields['name']
         if not name: raise KGLoaderException("Invalid/Missing name")
 
-        # set statement
+        # load the statement
         if get_statement:
+            logger.info("Loading the statement")
             task_fields['statements'] = {}
             lang = 'en'
             statement_path = os.path.join(self.path, task_config['statement'])
@@ -118,10 +145,11 @@ class KGTaskLoader(TaskLoader):
             task_fields['primary_statements'] = [lang]
             task_fields['statements'][lang] = Statement(lang, digest)
 
-        # set attachments
+        # load the attachments
         # TODO Python 3.8
         attachments = task_config.get('attachments', [])
         if attachments:
+            logger.info("Loading the attachments")
             task_fields['attachments'] ={}
             for attachment in attachments:
                 path = os.path.join(self.path, 'attachments', attachment)
@@ -134,6 +162,7 @@ class KGTaskLoader(TaskLoader):
 
         # set task-type-specific fields
         task_type = task_config['task_type']
+        logger.info(f"The task type is {task_type}")
         if task_type == 'Batch':
             task_fields['submission_format'] = [f'{name}.%l']
         else:
@@ -141,13 +170,14 @@ class KGTaskLoader(TaskLoader):
 
         # convert some fields to their required types
         for field in 'min_submission_interval', 'min_user_test_interval':
-            if isinstance(task_fields[field], int):
+            if isinstance(task_fields[field], (int, float)):
                 task_fields[field] = timedelta(seconds=task_fields[field])
 
         return Task(**task_fields)
 
     def _create_and_attach_dataset(self, task_config, task):
         # create dataset
+        logger.info("Creating the Dataset")
         dataset_fields = {field: task_config.get(field, default) for field, default in DATASET_DEFAULTS.items()}
         dataset_fields['task'] = task
         dataset_fields['description'] = "Default"
@@ -156,6 +186,7 @@ class KGTaskLoader(TaskLoader):
         # set checker
         checker_path = os.path.join(self.path, 'checker')
         if os.path.exists(checker_path):
+            logging.info("Loading the checker")
             make_executable(checker_path) # force it to be executable
             digest = self.file_cacher.put_file_from_path(checker_path, f"Checker for Task: {task.name}")
             dataset_fields['managers']['checker'] = Manager('checker', digest)
@@ -167,6 +198,7 @@ class KGTaskLoader(TaskLoader):
         dataset_fields['task_type_parameters'] = ['alone', ["", ""], evaluation_param]
 
         # read test data from tests/
+        logger.info("Reading the test data")
         test_bases = defaultdict(IOPair)
         tests_path = os.path.join(self.path, 'tests')
         for test_filename in os.listdir(tests_path):
@@ -179,6 +211,7 @@ class KGTaskLoader(TaskLoader):
             else:
                 raise KGLoaderException(f"Unrecognize file found in tests/: {test_filename}")
 
+        # check for missing I/O
         # TODO Python 3.8
         bad_io = [io_base for io_base, io_pair in test_bases.items() if not (io_pair.input and io_pair.output)]
         if bad_io:
@@ -187,6 +220,8 @@ class KGTaskLoader(TaskLoader):
         if not test_bases:
             raise KGLoaderException("tests/ must not be empty")
 
+        # load test cases
+        logger.info(f"Found {len(test_bases)} cases. Loading the test data")
         dataset_fields['testcases'] = {}
         for test_basename, test_base in sorted(test_bases.items()):
             dataset_fields['testcases'][test_basename] = Testcase(
@@ -202,12 +237,121 @@ class KGTaskLoader(TaskLoader):
             dataset_fields[field] = float(dataset_fields[field])
 
         dataset = Dataset(**dataset_fields)
+
+        # set it as the active dataset
         task.active_dataset = dataset
+
         return dataset
 
     def get_task(self, get_statement=True):
+        # load the task config file
         task_config = self._get_task_config()
+
+        # construct the task object
         task = self._get_task(task_config, get_statement=get_statement)
+
+        # attach the dataset
         self._create_and_attach_dataset(task_config, task)
+
         logger.info(f"Task {task.name!r} successfully loaded.")
         return task
+
+
+class KGContestLoader(ContestLoader, UserLoader):
+    """
+    Load a contest and users stored using the KompGen cms-compiled format.
+
+    Given the filesystem location of a contest prepared via KompGen and compiled
+    via `kg contest cms`, parse the contents to produce a CMS Task object.
+
+    Not all options are supported yet.
+    """
+
+    short_name = 'kg_contest'
+    description = 'KompGen CMS contest format'
+
+    @staticmethod
+    def detect(path):
+        if not os.path.isdir(path): path = os.path.dirname(path)
+        return os.path.exists(os.path.join(path, KG_CONTEST))
+
+    def user_has_changed(self):
+        return True
+
+    def contest_has_changed(self):
+        return True
+
+    def get_task_loader(self, taskname):
+        return KGTaskLoader(os.path.join(self.path, taskname), self.file_cacher)
+
+    def _get_contest_config(self):
+        kg_contest = os.path.join(self.path, KG_CONTEST)
+        logger.info(f"Reading {kg_contest}")
+        with open(kg_contest) as file:
+            return json.load(file)
+
+    def _get_user_list(self):
+        path = self.path if os.path.isdir(self.path) else os.path.dirname(self.path)
+        kg_users = os.path.join(path, KG_USERS)
+        logging.info(f"Reading {kg_users}")
+        with open(kg_users) as file:
+            return json.load(file)
+
+    def _get_minimal_participations(self):
+        participations = []
+        for user in self._get_user_list():
+            if user['type'] == 'user':
+                participations.append({
+                        'username': user['username'],
+                        'password': build_password(user['password']),
+                    })
+
+        return participations
+
+    def _get_contest(self, contest_config):
+
+        contest_fields = {field: contest_config.get(field, default) for field, default in CONTEST_DEFAULTS.items()}
+
+        # convert lang names
+        contest_fields['languages'] = [LANGUAGE_MAPPING.get(lang, lang) for lang in contest_fields['languages']]
+
+        # convert datetimes
+        for field in 'start', 'stop':
+            if isinstance(contest_fields[field], (int, float)):
+                contest_fields[field] = datetime.utcfromtimestamp(contest_fields[field])
+
+        # convert timedeltas
+        for field in 'min_submission_interval', 'min_user_test_interval':
+            if isinstance(contest_fields[field], (int, float)):
+                contest_fields[field] = timedelta(seconds=contest_fields[field])
+
+        return Contest(**contest_fields)
+
+    def get_user(self):
+        username = os.path.basename(self.path)
+        for user in self._get_user_list():
+            if user['type'] == 'user' and user['username'] == username:
+                return User(
+                        username=user['username'],
+                        password=build_password(user['password']),
+                        first_name=user['first_name'] or '',
+                        last_name=user['last_name'] or '',
+                    )
+        else:
+            raise KGLoaderException(f"Unknown user: {username}")
+
+    def get_contest(self):
+        # load the contest config data
+        contest_config = self._get_contest_config()
+
+        # construct the contest object
+        contest = self._get_contest(contest_config)
+
+        # get task list
+        tasks = contest_config['problems']
+
+        # get list of participations (usernames)
+        participations = self._get_minimal_participations()
+
+        logger.info(f"Contest {contest.name!r} successfully loaded.")
+        return contest, tasks, participations
